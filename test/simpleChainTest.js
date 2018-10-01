cfa2be323e0ebec53361634545feafe1e9fdca48
Devinder Khroad <dkhroad@gmail.com>
2018-10-01 15:13:26 -0700

    add tests for addBlock() and validateChain()

--- test/simpleChainTest.js
+++ test/simpleChainTest.js
@@ -6,32 +6,35 @@ const {
 
 const del = require('del');
 
-const chainTestDB = './chain_test_data'; 
+const chainTestDB = './test/chain_test_data'; 
 
-async function clearDB(db) {
-  return new Promise(function(resolve,reject) {
-    db.destroy(chainTestDB,function(error) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve([]);
-      }
+function dumpChain(bc) {
+  let i=0;
+  bc.chain.createReadStream()
+    .on('data',function(data) {
+      console.log('data',data);
+    }).on('close',function() {
+      console.log('Total',i);
+    }).on('error',function(error) {
+      console.log(error);
     });
-  });
 }
 
 describe('Blockchain', function() {
   let blockchain;
 
-  before(async function() {
+  beforeEach(async function() {
     del.sync(chainTestDB);
     blockchain = new Blockchain(chainTestDB); 
+  });
+
+  afterEach(async function() {
+    await blockchain.chain.close();
   });
 
 
   describe('#addGenesisBlockIfMissing', function() {
       it('add genesis block', async function() {
-      // let blockchain = new Blockchain(chainTestDB); 
         let orig_height = await blockchain.getBlockHeight();
         expect(orig_height,'height_before').to.equal(-1);
         await blockchain.addGenesisBlockIfMissing();
@@ -42,7 +45,7 @@ describe('Blockchain', function() {
       
       });
 
-      it('adds genesis block one time', async function() {
+      it('adds genesis block one time only', async function() {
         await blockchain.addGenesisBlockIfMissing();
         let curr_height =  await blockchain.getBlockHeight();
         expect(curr_height,'height_after').to.equal(0);
@@ -50,6 +53,66 @@ describe('Blockchain', function() {
         curr_height =  await blockchain.getBlockHeight();
         expect(curr_height,'height_after').to.equal(0);
       });
+  });
+
+  describe('#addBlock', function() {
+    it('adds block',async function() {
+      let bh = await blockchain.addBlock(new Block('test block'));
+      expect(bh,'current block height').to.equal(1);
+      lastBlock = await blockchain.getLastBlock();
+      expect(lastBlock.body,'block content').to.equal('test block');
+    });
+
+    it('adds multiple blocks in a lexicographical order',async function() {
+      let cbh = -2
+      for (let i=0;i<=10;i++) {
+         cbh = await blockchain.addBlock(new Block('Test block - #'+i));
+      }
+      console.log(cbh);
+      dumpChain(blockchain);
+      let lastBlock = await blockchain.getLastBlock();
+      expect(lastBlock.body,'block content').to.equal('Test block - #10');
+
+      expect(cbh,'height_after').to.equal(11);
+        
+    });
+
+    it('adds genesis block if missing',async function() {
+      let bh = await blockchain.addBlock(new Block('test block'));
+      firstBlock = await blockchain.getBlock(0);
+      expect(firstBlock.body,'block content').to.include('Genesis block');
+    });
+  });
+
+  describe('#validateChain',function() {
+    beforeEach(async function() {
+      for (let i=0;i<=10;i++) {
+        await blockchain.addBlock(new Block('Test block - #'+i));
+      }
+    });
+
+    it('validates good chain correctly',async function() {
+      errorLog = await blockchain.validateChain();
+      expect(errorLog.size).to.equal(0);
+    });
+
+    it('detects errors in a corrupt chain',async function() {
+      for (var i of [2,4,7]) {
+        var b = await blockchain.getBlock(i);
+        b.body = 'induce chain error -- ' + i;
+        await blockchain.chain.put(blockchain.lexi(i),JSON.stringify(b));
+      }
+      errorLog = await blockchain.validateChain();
+      expect(errorLog.size).to.equal(3);
+      let it = errorLog.values();
+      expect(it.next().value).to.equal(2);
+      expect(it.next().value).to.equal(4);
+      expect(it.next().value).to.equal(7);
+
+    });
+      
+      
+
   });
 
 
